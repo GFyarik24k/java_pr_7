fe3f57c99467e8693394b5f2b7411bb79a7302a1
Yaroslav Mazhey <yaroslav_1007@mail.ru>
2021-12-07 01:11:42 +0300

    Initial commit

---
+++ src/pr_7/CardGame.java
@@ -0,0 +1,60 @@
+package pr_7;
+
+import java.util.*;
+
+public class CardGame {
+    private Stack<Integer> Deck1 = new Stack<Integer>();
+    private Stack<Integer> Deck2 = new Stack<Integer>();
+    private int count = 1;
+    int a, b;
+    Scanner field = new Scanner(System.in);
+
+    public CardGame() {
+        for (int i = 0;i<5;i++)
+            Deck1.push(field.nextInt());
+        for (int i=0;i<5;i++)
+            Deck2.push(field.nextInt());
+    }
+
+    public void pushBack(Stack<Integer> t,int a){
+        Stack<Integer> temp = new Stack<Integer>();
+        while (t.empty() == false){
+            temp.push(t.pop());
+        }
+        temp.push(a);
+        while (temp.empty() == false){
+            t.push(temp.pop());
+        }
+    }
+
+    public void game() {
+        while (!Deck1.isEmpty() && !Deck2.isEmpty()) {
+            if (count ==106) {
+                System.out.print("botva");
+                break;
+            }
+            System.out.println("Ход №"+count);
+            a = Deck1.pop();
+            b = Deck2.pop();
+            if ((a > b || (a == 0 && b == 9)) && (a != 9 || b != 0)){
+                    System.out.println("В раздаче выиграл первый игрок!");
+                    pushBack(Deck1, a);
+                    pushBack(Deck1, b);
+                }
+            if ((b > a || (b == 0 && a == 9)) && (b != 9 || a != 0))
+            {
+                System.out.println("В раздаче выиграл второй игрок!");
+                pushBack(Deck2, a);
+                pushBack(Deck2, b);
+
+            }
+            count++;
+            System.out.println();
+        }
+        count--;
+        if (Deck1.isEmpty())
+            System.out.print("Выиграл второй игрок! За " + count + " раздач");
+        else if (Deck2.isEmpty())
+            System.out.print("Выиграл первый игрок! За " + count + " раздач");
+    }
+}
---
+++ src/pr_7/Main.java
@@ -0,0 +1,9 @@
+package pr_7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CardGame A = new CardGame();
+        A.game();
+    }
+}
